cfa0ebde16a111e528332ea3a4cae4d6902f0e62
Michelle Too <mjxtcodes@gmail.com>
2023-02-27 15:12:25 -0800

    updated command.sh

--- command.sh
+++ command.sh
@@ -1,5 +1,9 @@
+# npx create-react-app blackbird
+# cd blackbird
 # git remote add origin git@github.com:michelle-jx/forage-blackbird.git
 # git branch -M main
-# git push -u origin main
+# git add .
+# git commit -m 'update logo + link'
 # git checkout -b update_logo
+# git push origin update_logo
 # REPO_URL https://github.com/michelle-jx/forage-blackbird/tree/update_logo 
