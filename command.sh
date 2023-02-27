f1928d7132b5a88602e6a714aaafc35d99ce82d8
Michelle Too <mjxtcodes@gmail.com>
2023-02-27 15:08:13 -0800

    added command.sh

---
+++ command.sh
@@ -0,0 +1,5 @@
+# git remote add origin git@github.com:michelle-jx/forage-blackbird.git
+# git branch -M main
+# git push -u origin main
+# git checkout -b update_logo
+# REPO_URL https://github.com/michelle-jx/forage-blackbird/tree/update_logo 
