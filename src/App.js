3becf7535d0230127b366168d83e2ad01d98792c
Michelle Too <mjxtcodes@gmail.com>
2023-02-27 15:11:00 -0800

    fixed img src

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg" className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
